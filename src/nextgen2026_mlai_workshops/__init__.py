4cc40dc2450b2fe0b42bcc60d497d55efbc5e2e0
Sam Cantrill <sam.cantrill@gmail.com>
2026-05-19 21:12:40 +1000

    feat: checkpointed workshop plan

--- src/nextgen2026_mlai_workshops/__init__.py
+++ src/nextgen2026_mlai_workshops/__init__.py
@@ -1,5 +1,12 @@
 """Workshop package for the NextGen 2026 ML/AI workshops."""
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "show_polynomial_recap"]
 
 __version__ = "0.1.0"
+
+
+def show_polynomial_recap(*args, **kwargs):
+    """Lazy package-level access to the Notebook 00 recap plot."""
+    from .plots import show_polynomial_recap as _show_polynomial_recap
+
+    return _show_polynomial_recap(*args, **kwargs)
